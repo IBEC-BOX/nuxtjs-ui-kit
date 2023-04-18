e794fcd329b57324502dcb88231fc390e2b86950
Aidosgd <aidosgd@gmail.com>
2023-04-18 20:55:25 +0600

    Update table

--- src/components/Table.stories.js
+++ src/components/Table.stories.js
@@ -1,19 +1,19 @@
-import Table from './Table.vue';
-import Button from './Button.vue';
+import Table from "./Table.vue";
+import Button from "./Button.vue";
 
 // More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
 export default {
-  title: 'Some UI/Table',
+  title: "Some UI/Table",
   component: Table,
   // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
   argTypes: {
-    title: {control: 'text'},
-    total: {control: 'number'},
-    total_title: {control: 'text'},
-    headF: {control: 'array'},
-    items: {control: 'array'},
-    striped: {control: 'boolean'},
-    hover: {control: 'boolean'}
+    title: { control: "text" },
+    total: { control: "number" },
+    total_title: { control: "text" },
+    headF: { control: "array" },
+    items: { control: "array" },
+    striped: { control: "boolean" },
+    hover: { control: "boolean" },
   },
 };
 
@@ -23,71 +23,108 @@ const Template = (args, { argTypes }) => ({
   components: { Table, Button },
   template: `
   <Table v-bind="$props">
-<!--    <template #head(index)="{ data }">-->
-<!--      <Button @click="testFunction(data)">test</Button>-->
-<!--    </template>-->
+    <template #head(index)="{ data }">
+      <span @click="testFunction(data)"><b>INDEX SLOT HEAD</b></span>
+    </template>
+  
+    <template #cell(index)="{ data }">
+      <span @click="testFunction(data)"><b>INDEX SLOT BODY</b></span>
+    </template>
 
-<!--    <template #pre-head(index)="{ data }">-->
-<!--      <Button @click="testFunction(data)">pre test</Button>-->
-<!--    </template>-->
+    <template #cell(last_name)="{ data, field }">
+      <span @click="testFunction(data)">SLOT DATA {{data[field.key]}}</span>
+    </template>
 
-<!--    <template #pre-cell(last_name)="{ data }">-->
-<!--      <Button @click="testFunction(data)">pre test222</Button>-->
-<!--    </template>-->
-
-<!--    <template #cell(last_name)="{ data }">-->
-<!--      <Button @click="testFunction(data)">test222</Button>-->
-<!--    </template>-->
+    <template #cell(first_name)="{ data }">
+      <span @click="testFunction(data)">SLOT BODY</span>
+    </template>
   </Table>`,
   methods: {
-    testFunction(data){
-      console.log('222', data)
-    }
-  }
+    testFunction(data) {
+      console.log("222", data);
+    },
+  },
 });
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/vue/writing-stories/args
 Primary.args = {
-  title: 'Team members',
+  title: "Team members",
   total: 100,
-  total_title: 'users',
+  total_title: "users",
   headF: [
     {
-      key: 'index',
-      slotBody: 'cell(index)',
-      slotHead: 'head(index)',
+      key: "index",
     },
     {
-      key: 'last_name',
+      key: "last_name",
       sortable: true,
-      slotBody: 'cell(last_name)',
-      slotHead: 'head(last_name)',
     },
     {
-      key: 'first_name',
+      key: "first_name",
       sortable: false,
     },
     {
-      key: 'age',
-      label: 'Person age',
+      key: "age",
+      label: "Person age",
       sortable: true,
-      // Variant applies to the whole column, including the header and footer
-      variant: 'info',
     },
   ],
   items: [
-    { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+    { age: 40, first_name: "Dickerson", last_name: "Macdonald" },
     {
       age: 21,
-      first_name: 'Larsen',
-      last_name: 'Shaw',
-      _rowVariant: 'danger',
+      first_name: "Larsen",
+      last_name: "Shaw",
     },
-    { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-    { age: 38, first_name: 'Jami', last_name: 'Carney' },
+    { age: 89, first_name: "Geneva", last_name: "Wilson" },
+    { age: 38, first_name: "Jami", last_name: "Carney" },
   ],
   striped: false,
-  hover: false
+  hover: false,
 };
 
+export const WithSlots = Template.bind({});
+
+WithSlots.args = {
+  title: "Team members",
+  total: 100,
+  total_title: "users",
+  headF: [
+    {
+      key: "index",
+      slotBody: "cell(index)",
+      slotHead: "head(index)",
+    },
+    {
+      key: "last_name",
+      sortable: true,
+      slotBody: "cell(last_name)",
+    },
+    {
+      key: "first_name",
+      sortable: false,
+      slotBody: "cell(first_name)",
+    },
+    {
+      key: "age",
+      label: "Person age",
+      sortable: true,
+      // Variant applies to the whole column, including the header and footer
+      variant: "info",
+    },
+  ],
+  items: [
+    { age: 40, first_name: "Dickerson", last_name: "Macdonald" },
+    {
+      age: 21,
+      first_name: "Larsen",
+      last_name: "Shaw",
+      _rowVariant: "danger",
+    },
+    { age: 89, first_name: "Geneva", last_name: "Wilson" },
+    { age: 38, first_name: "Jami", last_name: "Carney" },
+  ],
+  striped: false,
+  hover: false,
+};
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,27 +1,46 @@
 <template>
   <div class="ui-table">
-    <div class="ui-table__title">{{ title }} <div v-if="total" class="ui-table__total">{{ total }} {{ total_title }}</div></div>
+    <div class="ui-table__title">
+      {{ title }}
+      <div v-if="total" class="ui-table__total">
+        {{ total }} {{ total_title }}
+      </div>
+    </div>
     <div class="table-responsive">
-      <table :class="{'table': true,  'table-striped': striped, 'table-hover': hover}">
+      <table
+        :class="{ table: true, 'table-striped': striped, 'table-hover': hover }"
+      >
         <thead>
           <tr>
-            <th :class="field.variant ? `table-${field.variant}` : null" v-for="field in headF" :key="field.key">
-              <slot :name="`pre-${field.slotHead}`" :data="field" />
-
-              {{ field.label || field.key.replaceAll('_', ' ') | capitalize }}
+            <th
+              :class="field.variant ? `table-${field.variant}` : null"
+              v-for="field in headF"
+              :key="field.key"
+            >
+              <template v-if="!field.slotHead">
+                {{ field.label || field.key.replaceAll("_", " ") | capitalize }}
+              </template>
 
               <slot :name="field.slotHead" :data="field" />
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr :class="item._rowVariant ? `table-${item._rowVariant}` : null" v-for="item in items" :key="item.id">
-            <td :class="field.variant ? `table-${field.variant}` : null" v-for="field in headF" :key="field.key">
-              <slot :name="`pre-${field.slotHead}`" :data="field" />
+          <tr
+            :class="item._rowVariant ? `table-${item._rowVariant}` : null"
+            v-for="item in items"
+            :key="item.id"
+          >
+            <td
+              :class="field.variant ? `table-${field.variant}` : null"
+              v-for="field in headF"
+              :key="field.key"
+            >
+              <template v-if="!field.slotBody">
+                {{ item[field.key] }}
+              </template>
 
-              {{ item[field.key] }}
-
-              <slot :name="field.slotBody" :data="item" />
+              <slot :name="field.slotBody" :data="item" :field="field" />
             </td>
           </tr>
         </tbody>
@@ -30,36 +49,43 @@
   </div>
 </template>
 
-
 <script>
-import filters from '../../src/mixins/filter'
+import filters from "../../src/mixins/filter";
 
 export default {
   name: "UiTable",
-  props: ['title', 'items', 'headF', 'total', 'total_title', 'striped', 'hover'],
+  props: [
+    "title",
+    "items",
+    "headF",
+    "total",
+    "total_title",
+    "striped",
+    "hover",
+  ],
   mixins: [filters],
-}
+};
 </script>
 
 <style lang="scss" scoped>
-.ui-table{
-  &__title{
+.ui-table {
+  &__title {
     font-weight: 500;
     font-size: 18px;
     line-height: 28px;
     color: #101828;
     margin-bottom: 19px;
   }
-  &__total{
+  &__total {
     display: inline-block;
     padding: 2px 8px;
-    background: #F9F5FF;
+    background: #f9f5ff;
     border-radius: 16px;
     font-weight: 500;
     font-size: 12px;
     line-height: 18px;
     text-align: center;
-    color: #6941C6;
+    color: #6941c6;
   }
 }
 .table {
@@ -76,4 +102,3 @@ export default {
   }
 }
 </style>
-
